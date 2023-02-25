a858e5618a996e4dfd726e883823e31c59297cb6
victorhcvp <victorhugovergueiro@gmail.com>
2023-02-25 13:20:06 -0400

    swagger api docs

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,8 @@
+import swaggerUi from "swagger-ui-express";
+
 import { app } from ".";
+import swaggerJson from "../swagger.json";
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerJson));
 
 app.listen(3333, () => console.log("Server is running!"));
